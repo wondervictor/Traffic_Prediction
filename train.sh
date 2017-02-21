2fa36c241b0d34cda52dcf7aa1acdf04f6f8c9ad
wondervictor <victorchanchina@gmail.com>
2017-02-21 22:24:16 +0800

    prepare trainer

--- train.sh
+++ train.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash
+set -e
+output=./out
+cfg=trainer_config.py
+paddle train \
+  --config=${cfg} \
+  --save_dir=./output \
+  --trainer_count=2 \
+  --log_period=100 \
+  --dot_period=10 \
+  --num_passes=10 \
+  --use_gpu=false \
+  --show_parameter_stats_period=3000 \
+  --save_dir=${output} \
+  2>&1 | tee 'train.log'
