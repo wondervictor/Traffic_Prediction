c6988c4a1fa270bf5fce776db92d956c127bab87
wondervictor <victorchanchina@gmail.com>
2017-02-26 15:36:07 +0800

    improve the network

--- train.sh
+++ train.sh
@@ -6,10 +6,10 @@ cfg=trainer_config.py
 paddle train \
   --config=$cfg \
   --save_dir=./output \
-  --trainer_count=1 \
+  --trainer_count=32 \
   --log_period=1000 \
-  --dot_period=10 \
-  --num_passes=50 \
+  --dot_period=100 \
+  --num_passes=1000 \
   --use_gpu=false \
   --show_parameter_stats_period=3000 \
   2>&1 | tee 'train.log'
--- trainer_config.py
+++ trainer_config.py
@@ -15,12 +15,10 @@ batch_size = 288
 
 settings(
     batch_size=batch_size,
-    learning_rate=0.001,
+    learning_rate=0.01,
     learning_method=RMSPropOptimizer(),
     regularization=L2Regularization(8e-4)
 )
-
-
 
 NODE_NUM = 3
 TERM_SIZE = 12
@@ -31,6 +29,8 @@ for i in range(NODE_NUM):
     inputs_data.append(data_layer(name=key, size=TERM_SIZE))
 
 label = data_layer(name='label', size=4)
+
+###### CONFIGURATION #######
 
 lstm_1_outputs = []
 lstm_pool_outputs = []
@@ -49,6 +49,7 @@ sim_lstm = simple_lstm(input=fc_hidden2, size=TERM_SIZE, act=ReluActivation())
 fc_hidden_pool = pooling_layer(input=sim_lstm, pooling_type=SumPooling())
 
 
+
 hidden1 = fc_layer(input=lstm_pool_outputs, size=NODE_NUM, act=ReluActivation())
 hidden2 = fc_layer(input=hidden1, size=NODE_NUM*NODE_NUM, act=ReluActivation())
 hidden3 = fc_layer(input=hidden2, size=TERM_SIZE, act=ReluActivation())
@@ -59,18 +60,18 @@ rnn = recurrent_layer(input=input_important_data, act=ReluActivation())
 lstm_fc_layers = []
 rnn_pool = pooling_layer(input=rnn, pooling_type=AvgPooling())
 
-
 for data in lstm_1_outputs:
     fc_1_layer = fc_layer(input=data, size=TERM_SIZE, act=TanhActivation())
     lstm_fc_layers.append(fc_1_layer)
 
 all_layer_1 = fc_layer(input=lstm_fc_layers, size=TERM_SIZE*NODE_NUM, act=ReluActivation())
-all_layer_2 = fc_layer(input=all_layer_1, size=TERM_SIZE*TERM_SIZE, act=ReluActivation())
+all_layer_2 = fc_layer(input=all_layer_1, size=TERM_SIZE*TERM_SIZE, act=ReluActivation(),layer_attr=layer_attr)
 all_layer = fc_layer(input=all_layer_2, size=TERM_SIZE, act=ReluActivation())
 
 hidden_4 = fc_layer(input=[rnn_pool, all_layer, hidden3, fc_hidden_pool], size=TERM_SIZE, act=ReluActivation())
+dropout_layer_1 = dropout_layer(input=hidden_4, dropout_rate=0.2)
 
-outLayer = fc_layer(input=hidden_4, size=4, act=SoftmaxActivation())
+outLayer = fc_layer(input=dropout_layer_1, size=4, act=SoftmaxActivation())
 
 outputs(classification_cost(input=outLayer, label=label))
 
