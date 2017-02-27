0639c13682b4efe5a272aec495e536f6c440aa29
wondervictor <victorchanchina@gmail.com>
2017-02-27 17:18:03 +0800

    create a new structure

---
+++ trainer_config.py
@@ -0,0 +1,104 @@
+from paddle.trainer_config_helpers import *
+is_predict = get_config_arg('is_predict', bool, False)
+
+define_py_data_sources2(
+    train_list='data/train.list',
+    test_list='data/test.list',
+    module='data_provider',
+    obj='process'
+    # args={
+    #     'num': 3
+    # }
+)
+
+batch_size = 288
+
+settings(
+    batch_size=batch_size,
+    learning_rate=0.01,
+    learning_method=RMSPropOptimizer(),
+    regularization=L2Regularization(8e-4)
+)
+
+NODE_NUM = 3
+TERM_SIZE = 12
+
+
+# input
+input_data = []
+for i in range(NODE_NUM):
+    key = "data_%s" % i
+    input_data.append(data_layer(name=key, size=TERM_SIZE))
+# label
+label = data_layer(name='label', size=4)
+
+# 1 - LSTM
+lstm_1_layer_outputs = []
+for data in input_data:
+    lstm_layer = lstmemory(input=data, act=ReluActivation())
+    lstm_1_layer_outputs.append(lstm_layer)
+
+# 1 - pool
+
+# 1 - pool - avg
+lstm_1_avg_pools = []
+for out in lstm_1_layer_outputs:
+    avg = pooling_layer(input=out, pooling_type=AvgPooling())
+    lstm_1_avg_pools.append(avg)
+
+# 1 - pool - last
+lstm_1_last_pools = []
+for out in lstm_1_layer_outputs:
+    last = last_seq(input=out)
+    lstm_1_last_pools.append(last)
+
+# 1 - lstm - output
+lstm_1_outputs = [lstm_1_last_pools, lstm_1_avg_pools]
+
+# 1 - fc
+fc_1_1_layer = fc_layer(input=lstm_1_outputs, size=TERM_SIZE, act=SigmoidActivation())
+fc_1_2_layer = fc_layer(input=fc_1_1_layer, size=TERM_SIZE*NODE_NUM, act=ReluActivation())
+
+# 1 all layers output
+all_1_layers = [lstm_1_last_pools, fc_1_2_layer]
+
+
+# 2 - fc
+fc_2_1_layer = fc_layer(input=input_data, size=TERM_SIZE, act=ReluActivation())
+fc_2_2_layer = fc_layer(input=fc_2_1_layer, size=TERM_SIZE*NODE_NUM, act=TanhActivation())
+
+# 2 - simple lstm
+simple_2_1_lstm = simple_lstm(input=fc_2_2_layer, size=TERM_SIZE*NODE_NUM, act=ReluActivation())
+
+# 2 - fc
+fc_2_3_layer = fc_layer(input=simple_2_1_lstm, size=TERM_SIZE, act=ReluActivation())
+
+# 2 - simple lstm
+simple_2_2_lstm = simple_lstm(input=fc_2_3_layer, size=TERM_SIZE, act=ReluActivation())
+
+# 2 - last pool
+last_2_pool = last_seq(input=simple_2_2_lstm)
+
+# all 2 layers output
+
+all_2_layers = last_2_pool
+
+all_ouputs = []
+all_ouputs.extend(all_1_layers)
+all_ouputs.append(all_2_layers)
+
+all_fc_1_layer = fc_layer(input=all_ouputs, size=TERM_SIZE, act=ReluActivation())
+
+output_layer = fc_layer(input=all_fc_1_layer, size=4, act=SoftmaxActivation())
+
+cost = classification_cost(input=output_layer, label=label)
+
+outputs(cost)
+
+
+
+
+
+
+
+
