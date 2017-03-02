dd2a0ccdc0518362d6fa406b83ded88ed43ae48e
wondervictor <victorchanchina@gmail.com>
2017-03-02 23:37:07 +0800

    add predict

--- data_provider.py
+++ data_provider.py
@@ -117,12 +117,30 @@ def process(settings, filename):
             yield result
 
 
-
-
 def predict_initialize(settings, num, point, **kwargs):
-    data = []
+    s = dict()
+    settings.point = point
+    settings.num = num
+    for i in range(num):
+        key = 'data_%s' % i
+        s[key] = dense_vector_sequence(24)
 
 
+@provider(init_hook=predict_initialize,cache=CacheType.CACHE_PASS_IN_MEM)
+def process_predict(settings, filename):
+    with open(filename,'r') as f:
+        data = []
+        max_len = 0
+        node_num = settings.num
+        result = dict()
+        for line in f.readlines():
+            speeds = map(int, line.rstrip('\n').split(','))
+            data.append(speeds)
+            max_len = len(speeds)
+        for i in range(node_num):
+            key = 'data_%s' % i
+            result[key] = [[data[i][k] - 1 for k in range(max_len-TERM_SIZE-1, max_len-1)]]
+        yield result
 
 
     # for file_name in files:
