80201c0012983b94d0aa8e29d79bec2a16cce4a1
wondervictor <victorchanchina@gmail.com>
2017-03-30 07:52:39 +0800

    completely new model (contains bugs)

--- trainer_config.py
+++ trainer_config.py
@@ -1,4 +1,5 @@
 from paddle.trainer_config_helpers import *
+import numpy as np
 import logging
 
 is_predict = get_config_arg('is_predict', bool, False)
@@ -46,20 +47,6 @@ settings(
 )
 
 
-def forward_network(index, input_layer, output, size):
-    paramAttr = ParameterAttribute(name='common', initial_max=1.0, initial_min=-1.0)
-    key = 'label_%s' % index
-    label = data_layer(name=key, size=4)
-    recent_layer = fc_layer(input=output, size=size, act=ReluActivation())
-    fc_nn_layer = fc_layer(input=[recent_layer, output], act=ReluActivation, size=size*2, paramAttr=paramAttr)
-    dropout_1_layer = dropout_layer(input=fc_nn_layer, dropout_rate=0.2)
-    output_layer = fc_layer(input=dropout_1_layer, size=4, act=SoftmaxActivation())
-    time_output = classification_cost(input=output_layer, label=label)
-    time_value = maxid_layer(output_layer)
-    output.append(time_value)
-    return time_output, output
-
-
 
 TERM_SIZE = 24
 NODE_NUM = num
@@ -73,45 +60,67 @@ for i in range(NODE_NUM):
 output = []
 cost_output = []
 
-paramAttr = ParameterAttribute(name='common', initial_max=1.0, initial_min=-1.0)
-
 
 # 1 lstmemory cells
 lstm_cells = []
 
 for data in input_data:
-    lstm_cell = lstmemory(input=data, gate_act=TanhActivation(), act=ReluActivation(), state_act=TanhActivation(),size=1)
+    lstm_cell = lstmemory(input=data, gate_act=TanhActivation(), act=ReluActivation(), state_act=TanhActivation())
     lstm_cells.append(lstm_cell)
 
-lstm_fc_layer = fc_layer(input=lstm_cells, size=3, act=SigmoidActivation(),param_attr=paramAttr)
+lstm_fc_layer = fc_layer(input=lstm_cells, size=3, act=SigmoidActivation())
 
-simple_lstm_layer = simple_lstm(input=lstm_fc_layer, size=TERM_SIZE,paramAttr=paramAttr, gate_act=TanhActivation(), state_act=TanhActivation(),
-                                act=SigmoidActivation())
+simple_lstm_layer = simple_lstm(input=lstm_fc_layer,
+                                size=TERM_SIZE,
+                                gate_act=TanhActivation(),
+                                state_act=TanhActivation(),
+                                act=ReluActivation())
 lastseq_1_layer = last_seq(input=simple_lstm_layer)
 
-dropout_1_layer = dropout_layer(input=last_seq, dropout_rate=0.2)
+dropout_1_layer = dropout_layer(input=lastseq_1_layer, dropout_rate=0.2)
 
-time_1_output_layer = fc_layer(input=dropout_1_layer, size=4, act=SoftmaxActivation(), paramAttr=paramAttr)
+time_1_output_layer = fc_layer(input=dropout_1_layer, size=4, act=SoftmaxActivation())
 
-label_1 = data_layer(name='label_1', size=4)
+label_1 = data_layer(name='label_0', size=4)
 
-time_1_output = classification_cost(input=time_1_output_layer, label=label_1)
+time_1_cost = classification_cost(name='<--cost0-->', input=time_1_output_layer, label=label_1)
 
 time_1_value = maxid_layer(input=time_1_output_layer)
 
-cost_output.append(time_1_output)
-output.append(time_1_value)
+cost_output.append(time_1_cost)
+output.append(time_1_output_layer)
+
 
 for i in range(1, TERM_SIZE):
-    time_output, output_arr = forward_network(i, last_seq, output, TERM_SIZE)
+
+    former_output = output
+    paramAttr = ParameterAttribute(initial_max=1.0, initial_min=-1.0)
+
+    key = 'label_%s' % i
+
+    # former_input = np.mat(former_output)
+
+    label = data_layer(name=key, size=4)
+
+
+
+    recent_layer = fc_layer(input=former_output, size=TERM_SIZE, act=ReluActivation())
+
+    fc_nn_layer = fc_layer(input=[lastseq_1_layer, recent_layer], act=ReluActivation(), size=TERM_SIZE*2)
+
+    dropout_nn_layer = dropout_layer(input=fc_nn_layer, dropout_rate=0.2)
+
+    output_layer = fc_layer(input=dropout_nn_layer, size=4, act=SoftmaxActivation())
+
+    time_output = classification_cost(name='<--cost%s-->' % i, input=output_layer, label=label)
+
+    time_value = maxid_layer(output_layer)
+
+    output.append(output_layer)
+
     cost_output.append(time_output)
-    output = output_arr
 
 outputs(cost_output)
-
-
-
-
 
 
 
