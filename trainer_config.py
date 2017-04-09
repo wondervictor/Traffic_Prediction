7b539564cb46a25b5e8b2ccaff21cbee51a40aed
wondervictor <victorchanchina@gmail.com>
2017-04-09 11:30:48 +0800

    reduce the layers of LSTM

--- trainer_config.py
+++ trainer_config.py
@@ -41,7 +41,7 @@ if is_predict:
 
 settings(
     batch_size=batch_size,
-    learning_rate=0.001,
+    learning_rate=0.0001,
     learning_method=MomentumOptimizer(0.0001),
     regularization=L2Regularization(8e-4)
 )
@@ -67,8 +67,7 @@ for i in range(NODE_NUM):
 #     embeddings.append(embedding_layer(input=input_data[i], size=TERM_SIZE))
 input_fc_1_layer = fc_layer(input=input_data, size=NODE_NUM, act=ReluActivation())
 
-
-input_fc_2_layer = fc_layer(input=input_data, size=NODE_NUM*4, act=ReluActivation())
+input_fc_2_layer = fc_layer(input=input_fc_1_layer, size=NODE_NUM*4, act=ReluActivation())
 
 input_lstm_layer = lstmemory(input=input_fc_2_layer, act=ReluActivation())
 
@@ -78,7 +77,7 @@ drop_1_layer = dropout_layer(input=input_aggrerate, dropout_rate=0.1)
 
 fc_2_layer = fc_layer(input=drop_1_layer, size=NODE_NUM*NODE_NUM, act=ReluActivation())
 
-# lstm_2_layer = simple_lstm(input=fc_2_layer, size=NODE_NUM*NODE_NUM, act=ReluActivation())
+lstm_2_layer = simple_lstm(input=fc_2_layer, size=NODE_NUM*NODE_NUM, act=ReluActivation())
 #
 # con_layers = concat_layer(input=[fc_2_layer, lstm_2_layer])
 #
@@ -90,15 +89,25 @@ fc_2_layer = fc_layer(input=drop_1_layer, size=NODE_NUM*NODE_NUM, act=ReluActiva
 # cost = classification_cost(input=first_timestamp_value, name='cost0', label=data_layer(name='label_0', size=4))
 # costs.append(cost)
 
-con_layers = fc_2_layer
+input_concat = concat_layer(input=input_data)
+con_layers = concat_layer(input=[input_concat, lstm_2_layer])
 
-for i in range(0, 24):
+labels = []
+
+for i in range(TERM_SIZE):
+    labels.append(data_layer('label_%s' % i, size=4))
+
+SIZE = TERM_SIZE
+
+for i in range(0, TERM_SIZE):
     fc_tmp_layer = fc_layer(input=con_layers, size=NODE_NUM * 4, act=TanhActivation())
-    lstm_tmp_layer = lstmemory(input=fc_tmp_layer, act=ReluActivation())
-    con_layers = concat_layer(input=[fc_tmp_layer, lstm_tmp_layer])
+    con_layers = concat_layer(input=[fc_tmp_layer, input_concat])
+    if i % 2 == 0:
+        lstm_tmp_layer = simple_lstm(input=fc_tmp_layer, size=NODE_NUM*NODE_NUM, act=ReluActivation())
+        con_layers = concat_layer(input=[fc_tmp_layer, lstm_tmp_layer, input_concat])
     result_aggrerate_layer = last_seq(con_layers)
     drop_tmp_layer = dropout_layer(input=result_aggrerate_layer, dropout_rate=0.1)
     time_value = fc_layer(input=drop_tmp_layer, size=4, act=SoftmaxActivation())
-    ecost = classification_cost(input=time_value, name='cost%s'%i, label=data_layer('label_%s'%i, size=4))
+    ecost = classification_cost(input=time_value, name='cost%s' % i, label=labels[i])
     costs.append(ecost)
 outputs(costs)
