d7d87d7a7239c206d72c98bc478120437bcd2541
wondervictor <victorchanchina@gmail.com>
2017-04-15 12:32:08 +0800

    extend nodes

---
+++ data/GraphProcess.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+extend the surrounded node ranges.
+
+1 -> 2 ->center ->3 -> 4
+"""
+
+def create_link_list(filename):
+    graph = {}
+    with open(filename, 'r') as open_file:
+        po
