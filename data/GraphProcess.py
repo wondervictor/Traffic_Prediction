2b83b0b226794e6967211a45025aff5e19c23e13
wondervictor <victorchanchina@gmail.com>
2017-04-16 23:10:13 +0800

    creat new point list

--- data/GraphProcess.py
+++ data/GraphProcess.py
@@ -8,25 +8,29 @@ extend the surrounded node ranges.
 
 def create_link_list(filename):
     graph = {}
-    all_nodes = {}
     with open(filename, 'r') as open_file:
         for line in open_file.readlines():
             pair_points = map(int, line.rstrip('\r\n').split(','))
+
             key = '%s' % (pair_points[0])
             if key in graph:
                 point_list = graph[key]
-                point_list.append(pair_points[1])
+                if pair_points[1] not in point_list:
+                    point_list.append(pair_points[1])
                 graph[key] = point_list
             else:
                 graph[key] = [pair_points[1]]
+
             key2 = '%s' % pair_points[1]
             if key2 in graph:
                 point_list = graph[key2]
-                point_list.append(pair_points[0])
+                if pair_points[0] not in point_list:
+                    point_list.append(pair_points[0])
                 graph[key2] = point_list
             else:
                 graph[key2] = [pair_points[0]]
 
+    all_nodes = []
     for key in graph:
         point_list = graph[key]
         nodes = {}
@@ -37,20 +41,19 @@ def create_link_list(filename):
         for point in point_list:
             key = '%s' % point
             if key in graph:
-                subpoint_list = graph['%s' % point ]
-                subnodes.extend(subpoint_list)
-        while center_node in subnodes:
-            subnodes.remove(center_node)
+                subpoint_list = graph['%s' % point]
+                for subpoint in subpoint_list:
+                    if subpoint not in subnodes and subpoint not in point_list and subpoint != center_node:
+                        subnodes.append(subpoint)
         nodes['2'] = subnodes
-        all_nodes[key] = nodes
+        all_nodes.append(nodes)
 
     with open('two_dist_point', 'w+') as writingFile:
-        for key in all_nodes:
-            value = graph[key]
-            line = '%s;' % key
-            line += ','.join(['%s' % k for k in value['1']])
+        for node in all_nodes:
+            line = '%s;' % node['0']
+            line += ','.join(['%s' % k for k in node['1']])
             line += ';'
-            line += ','.join(['%s' % k for k in value['2']])
+            line += ','.join(['%s' % k for k in node['2']])
             line += '\n'
             writingFile.write(line)
 
